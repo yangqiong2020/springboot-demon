60a15a5e55e4f19e70aa3151b2197ae672e66830
yangqiong <364727769@qq.com>
2019-11-25 22:45:01 +0800

    1.增加注释提醒：springBoot启动类，必须放到controller的同级目录或上级目录，不然加载不到requestMappling中的url

--- 01-spring-boot-hello/src/main/java/yq/App.java
+++ 01-spring-boot-hello/src/main/java/yq/App.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * springBoot启动类
+ * 启动类要放在controller的同级目录或上级目录，不然加载不到requestMapping
  */
 @SpringBootApplication
 public class App {
