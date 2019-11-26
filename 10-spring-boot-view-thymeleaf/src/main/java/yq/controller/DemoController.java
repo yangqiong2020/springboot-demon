05102b2d184b4fb067bae37da31aec0ec2776a20
yangqiong <364727769@qq.com>
2019-11-26 16:02:58 +0800

    spring-data整合thymeleaf 1.增加了字符串的常用方法 2.增加了日期格式化用法

--- 10-spring-boot-view-thymeleaf/src/main/java/yq/controller/DemoController.java
+++ 10-spring-boot-view-thymeleaf/src/main/java/yq/controller/DemoController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+
 /**
  * thymeleaf 案例
  */
@@ -12,6 +14,7 @@ public class DemoController {
     @RequestMapping("/show")
     public String showInfo(Model model){
         model.addAttribute("msg","thymeleaf......");
+        model.addAttribute("key",new Date());
                 return "index";
     }
 }
