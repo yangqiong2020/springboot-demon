25452abcf4dc13f9cc021e8afd179ccb01069d1f
yangqiong <364727769@qq.com>
2019-11-26 16:34:29 +0800

    spring-data整合thymeleaf 1.增加了list遍历 2.增加了map遍历

--- 10-spring-boot-view-thymeleaf/src/main/java/yq/controller/DemoController.java
+++ 10-spring-boot-view-thymeleaf/src/main/java/yq/controller/DemoController.java
@@ -3,8 +3,9 @@ package yq.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import yq.pojo.Users;
 
-import java.util.Date;
+import java.util.*;
 
 /**
  * thymeleaf 案例
@@ -23,5 +24,25 @@ public class DemoController {
         model.addAttribute("sex" ,"男");
         model.addAttribute("num",1);
         return "index2";
+    }
+
+    @RequestMapping("/show3")
+    public String showInfo3(Model model){
+        List<Users> list = new ArrayList<>();
+        list.add(new Users(1,20,"张三"));
+        list.add(new Users(2 ,22,"李四"));
+        list.add(new Users(3,24,"王五"));
+        model.addAttribute("list", list);
+        return "index3";
+    }
+
+    @RequestMapping("/show4")
+    public String showInfo4(Model model){
+        Map<String, Users> map = new HashMap<>();
+        map.put("u1", new Users(1,21,"张三1"));
+        map.put("u2", new Users(1,21,"张三2"));
+        map.put("u3", new Users(1,23,"张三3"));
+        model.addAttribute("map", map);
+        return "index4";
     }
 }
---
+++ 10-spring-boot-view-thymeleaf/src/main/java/yq/pojo/Users.java
@@ -0,0 +1,46 @@
+package yq.pojo;
+
+public class Users {
+    private Integer userid;
+    private Integer userage;
+    private String username;
+
+    public Users(Integer userid, Integer userage, String username) {
+        this.userid = userid;
+        this.userage = userage;
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userid=" + userid +
+                ", userage=" + userage +
+                ", username='" + username + '\'' +
+                '}';
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getUserage() {
+        return userage;
+    }
+
+    public void setUserage(Integer userage) {
+        this.userage = userage;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
