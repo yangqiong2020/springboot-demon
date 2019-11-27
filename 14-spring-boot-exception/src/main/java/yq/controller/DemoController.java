dedf8e9411c4dcfc476a5e27ae28affdf9a6d1d3
yangqiong <364727769@qq.com>
2019-11-27 11:24:31 +0800

    spring-boot-exception 1.使用@ExceptionHandler 注解来捕获异常并且返回指定错误页面

--- 14-spring-boot-exception/src/main/java/yq/controller/DemoController.java
+++ 14-spring-boot-exception/src/main/java/yq/controller/DemoController.java
@@ -1,7 +1,9 @@
 package yq.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 处理异常方式一 自定义错误页面
@@ -20,4 +22,34 @@ public class DemoController {
         int a=10/0;
         return "index";
     }
+
+    /**
+     * java.lang.ArithmeticException
+     * 该方法需要返回一个ModelAndVIiew，目的是可以让我们封装异常信息以及视图的指定
+     * 参数Exception e 会将产生的异常对象注入到方法中
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value ={ java.lang.ArithmeticException.class})
+    public ModelAndView arithmeticExceptionHandler(Exception e){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("error",e.toString());
+        mv.setViewName("error1");
+        return mv;
+    }
+    /**
+     * java.lang.NullPointerException
+     * 该方法需要返回一个ModelAndVIiew，目的是可以让我们封装异常信息以及视图的指定
+     * 参数Exception e 会将产生的异常对象注入到方法中
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value={java.lang.NullPointerException.class})
+    public ModelAndView nullPointerExceptionHandler(Exception e){
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("error",e.toString());
+        mv.setViewName("error2");
+        return mv;
+    }
+
 }
