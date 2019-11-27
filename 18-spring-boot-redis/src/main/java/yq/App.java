d6928072dff4795a01c8e300300cdcc9a08a08b4
yangqiong <364727769@qq.com>
2019-11-27 16:17:30 +0800

    spring-boot-redis 1.测试与redis的连通性

---
+++ 18-spring-boot-redis/src/main/java/yq/App.java
@@ -0,0 +1,17 @@
+package yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ *spring-boot与redis整合
+ *
+ */
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
---
+++ 18-spring-boot-redis/src/test/java/yq/test/RedisTest.java
@@ -0,0 +1,40 @@
+package yq.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import yq.App;
+
+
+/**
+ * Spring Data Redis测试
+ *
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes= App.class)
+public class RedisTest {
+
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+	
+	/**
+	 * 添加一个字符串
+	 */
+	@Test
+	public void testSet(){
+		this.redisTemplate.opsForValue().set("key", "111111");
+	}
+	
+	/**
+	 * 获取一个字符串
+	 */
+	@Test
+	public void testGet(){
+		String value = (String)this.redisTemplate.opsForValue().get("key");
+		System.out.println(value);
+	}
+}
