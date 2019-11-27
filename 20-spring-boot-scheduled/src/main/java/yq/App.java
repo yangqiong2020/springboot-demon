d0a9301b0f075343186a7742f95e8724f6978bfc
yangqiong <364727769@qq.com>
2019-11-27 18:42:26 +0800

    spring-boot-scheduled 1.定时任务简单案例@Scheduled(cron="0/2 * * * * ?") 每两秒执行一次 2.开启定时任务@EnableScheduling注解

---
+++ 20-spring-boot-scheduled/src/main/java/yq/App.java
@@ -0,0 +1,19 @@
+package yq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 
+ *Scheduled
+ *
+ */
+@SpringBootApplication
+@EnableScheduling
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
